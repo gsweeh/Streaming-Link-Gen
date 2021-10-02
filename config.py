9ac882f3077e0a56f5347ae9681d705395f575be
gsweeh <gsweeh@gmail.com>
2021-10-02 15:19:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2022219715:AAG6r4RuYaLnG-Fmib6MKl4cHDU9AnQrOvU")
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 1234567))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", 2923258))
+    API_HASH = os.environ.get("API_HASH", "4c843703f04453d401e32b130e7425f5")
     # Your Updates channel username 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", https://t.me/Movie_leecher)
     # token.pickle file link
-    TOKEN_PICKLE = os.environ.get("TOKEN_PICKLE", None)
+    TOKEN_PICKLE = os.environ.get("TOKEN_PICKLE", https://fxstream.herokuapp.com/29556)
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Google drive folder id
-    parent_id = os.environ.get("GDRIVE_FOLDER_ID", "")
+    parent_id = os.environ.get("GDRIVE_FOLDER_ID", "0AAyyYrqhIe2qUk9PVA")
     # Set True is drive folder id is Team Drive
-    IS_TEAM_DRIVE = os.environ.get("IS_TEAM_DRIVE", "False")
+    IS_TEAM_DRIVE = os.environ.get("IS_TEAM_DRIVE", "True")
     if IS_TEAM_DRIVE.lower() == 'true':
         IS_TEAM_DRIVE = True
     else:
@@ -28,4 +28,4 @@ class Config(object):
     else:
         USE_SERVICE_ACCOUNTS = False
     # Your gdrive index url (Important)
-    INDEX_URL = os.environ.get("INDEX_URL", "")
+    INDEX_URL = os.environ.get("INDEX_URL", "https://movieleecher.movieleecher.workers.dev/0")
